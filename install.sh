6750667fb8ec3aff999d7003425313007ba9af7d
ViViDboarder <ViViDboarder@gmail.com>
2014-04-15 10:35:38 -0700

    Add build target for executing test classes

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ mkdir -p "$SUBLIME_DIR"
 cp -r "./ForceDotCom" "$SUBLIME_DIR/"
 
 cat ./ForceDotCom/AbuseTheForce.sublime-build | sed "s#"'${path}'"#$PATH#"  > "$SUBLIME_DIR/ForceDotCom/AbuseTheForce.sublime-build"
+cat ./ForceDotCom/AbuseTheForceTest.sublime-build | sed "s#"'${path}'"#$PATH#"  > "$SUBLIME_DIR/ForceDotCom/AbuseTheForceTest.sublime-build"
 
 echo "SFDC Sublime Plugin install finished"
 
